3f8543468f8a4ee655cb9593d3b0c9197d394314
Mateusz <golonkamateusz99@gmail.com>
2023-08-11 21:21:11 +0200

    Secound Commit Readme

--- main.py
+++ main.py
@@ -1,51 +1,83 @@
 import re
 from collections import Counter
 
+def count_words(text):
+    """
+    Counts the occurrence of individual words in the text.
 
-def count_words(text) :
+    Args:
+        text (str): The input text to be analyzed.
+
+    Returns:
+        Counter: A Counter object containing word frequencies.
+    """
     words = re.findall(r'\b\w+\b', text.lower())
     word_count = Counter(words)
     return word_count
 
+def average_sentence_length(text):
+    """
+    Calculates the average sentence length in the text.
 
-def average_sentence_length(text) :
+    Args:
+        text (str): The input text to be analyzed.
+
+    Returns:
+        float: The average sentence length in words.
+    """
     sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', text)
     sentence_lengths = [len(re.findall(r'\b\w+\b', sentence)) for sentence in sentences]
 
-    if len(sentence_lengths) > 0 :
+    if len(sentence_lengths) > 0:
         average_length = sum(sentence_lengths) / len(sentence_lengths)
         return average_length
-    else :
+    else:
         return 0
 
+def most_common_phrases(text, num_phrases=5):
+    """
+    Finds and returns the most commonly occurring phrases.
 
-def most_common_phrases(text, num_phrases=5) :
+    Args:
+        text (str): The input text to be analyzed.
+        num_phrases (int): The number of most common phrases to return.
+
+    Returns:
+        list: A list of tuples containing phrases and their frequencies.
+    """
     words = re.findall(r'\b\w+\b', text.lower())
-    phrases = [words[i :i + 2] for i in range(len(words) - 1)]
+    phrases = [words[i:i + 2] for i in range(len(words) - 1)]
     phrase_count = Counter(tuple(phrase) for phrase in phrases)
 
     return phrase_count.most_common(num_phrases)
 
+def count_letter_frequency(text):
+    """
+    Counts the frequency of individual letters in the text.
 
-def count_letter_frequency(text) :
+    Args:
+        text (str): The input text to be analyzed.
+
+    Returns:
+        Counter: A Counter object containing letter frequencies.
+    """
     letters = re.findall(r'[a-zA-Z]', text.lower())
     letter_count = Counter(letters)
     return letter_count
 
-
-if __name__ == "__main__" :
+if __name__ == "__main__":
     file_path = "sample_text_file.txt"
 
-    try :
-        with open(file_path, "r") as file :
+    try:
+        with open(file_path, "r") as file:
             sample_text = file.read()
-    except FileNotFoundError :
+    except FileNotFoundError:
         print(f"Nie można znaleźć pliku: {file_path}")
         exit()
 
     print("Słowa i ich wystąpienia:")
     word_count = count_words(sample_text)
-    for word, count in word_count.items() :
+    for word, count in word_count.items():
         print(f"{word}: {count}")
 
     print("\nŚrednia długość zdań:")
@@ -54,10 +86,10 @@ if __name__ == "__main__" :
 
     print("\nNajczęściej występujące wyrażenia:")
     common_phrases = most_common_phrases(sample_text)
-    for phrase, count in common_phrases :
+    for phrase, count in common_phrases:
         print(f"{', '.join(phrase)}: {count}")
 
     print("\nCzęstotliwość występowania liter:")
     letter_frequency = count_letter_frequency(sample_text)
-    for letter, count in letter_frequency.items() :
+    for letter, count in letter_frequency.items():
         print(f"{letter}: {count}")
